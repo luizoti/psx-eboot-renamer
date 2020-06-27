4710b8f911c0014bfe7dc933b611ffb681cce143
luizoti <luizlazoti@hotmail.com>
2020-06-27 20:54:11 -0300

    Logica do script atualizada

--- ebootrename.py
+++ ebootrename.py
@@ -2,10 +2,15 @@ import os
 import re
 import sys
 import argparse
+from gamenamedb import *
+
+partids = ['SLUS', 'SLPS', 'SLPM', 'SLKA', 'SLES', 'SIPS', 'SCUS', 'SCPS', 'SCES', 'SCED', 'SCAJ', 'LSP', 'HPS', 'ESPM', 'CPCS']
 
 parser = argparse.ArgumentParser()
 parser.add_argument("PATH", help="the path of eboot files", type=str)
 args = parser.parse_args()
+
+regex = "(?:\_)(S\w{2}S\_\d{5})"
 
 class EbootRename(object):
     """docstring for EbootRename"""
@@ -13,41 +18,41 @@ class EbootRename(object):
         self.ebootid = ''
         self.ebpaths = []
         self.listfiles()
-        self.getid()
+        self.getebinfo()
 
 
     def listfiles(self):
         for file in os.listdir(args.PATH):
             if '.pbp' in file.lower() :
-                file = args.PATH + file
+                file = f"{args.PATH}/{file}"
                 if file not in self.ebpaths:
                     self.ebpaths.append(file)
 
 
-    def getid(self):
+    def getebinfo(self):
         for gameeboot in self.ebpaths:
             try:
                 with open(gameeboot, "rb") as f:
                     n = 0
                     b = f.read(16)
-
-                    for x in range(0,5000):
+                    for number in range(0,5000):
                         ac2 = str("".join([chr(i) if 32 <= i <= 127 else "." for i in b])) # ascii string; chained comparison
-
                         if not ac2.count('.') > 6:
-                            if re.search("\_S\w{2}S\_\d{5}", ac2):
-                                self.ebootid = re.findall("\_S\w{2}S\_\d{5}", ac2)
-                                self.showname()
-                                pass
-
+                            if re.search(regex, ac2):
+                                self.ebootid = re.findall(regex, ac2)
+                                self.ebootid = self.ebootid[0].replace('_', '-')
+                                for _id in partids:
+                                    for i, n in eval(_id).items():
+                                        if i == self.ebootid:
+                                            self.rename(n)
                         n += 1
                         b = f.read(16)
             except Exception as e:
-                print(__file__, ": ", type(e).__name__, " - ", e, sep="", file=sys.stderr)
+                # print(__file__, ": ", type(e).__name__, " - ", e, sep="", file=sys.stderr)
                 pass
         
-    def showname(self):
-        print(self.ebootid)
+    def rename(self, n):
+        print(self.ebootid, n)
         pass
 
 if __name__ == '__main__':
