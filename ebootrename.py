4f4d46cc8e2891c06c2dd8eb2008498afc3775e5
luizoti <luizlazoti@hotmail.com>
2020-06-29 10:43:45 -0300

    regex and script updates

--- ebootrename.py
+++ ebootrename.py
@@ -4,13 +4,29 @@ import sys
 import argparse
 from gamenamedb import *
 
-partids = ['SLUS', 'SLPS', 'SLPM', 'SLKA', 'SLES', 'SIPS', 'SCUS', 'SCPS', 'SCES', 'SCED', 'SCAJ', 'LSP', 'HPS', 'ESPM', 'CPCS']
+partids = ['SLUS',
+            'SLPS',
+             'SLPM',
+              'SLKA',
+               'SLES',
+                'SIPS',
+                 'SCUS',
+                  'SCPS',
+                   'SCES',
+                    'SCED',
+                     'SCAJ',
+                      'LSP',
+                       'HPS',
+                        'ESPM',
+                         'CPCS']
 
 parser = argparse.ArgumentParser()
 parser.add_argument("PATH", help="the path of eboot files", type=str)
+parser.add_argument("-n", "--name", help="only name", action="store_true")
+parser.add_argument("-id", "--idend", help="id at the end", action="store_true")
 args = parser.parse_args()
 
-regex = "(?:\_)(S\w{2}S\_\d{5})"
+regex = "(?:\_)((?:S|L|H|E|C).+?(?:S|M|A|D|J|P|M)\_\d{5})"
 
 class EbootRename(object):
     """docstring for EbootRename"""
@@ -23,36 +39,52 @@ class EbootRename(object):
 
     def listfiles(self):
         for file in os.listdir(args.PATH):
-            if '.pbp' in file.lower() :
-                file = f"{args.PATH}/{file}"
+            if '.pbp' in file.lower():
+                file = os.path.join(args.PATH, file)
                 if file not in self.ebpaths:
                     self.ebpaths.append(file)
 
 
     def getebinfo(self):
         for gameeboot in self.ebpaths:
+            self.ebfilepath = gameeboot
             try:
-                with open(gameeboot, "rb") as f:
-                    n = 0
-                    b = f.read(16)
-                    for number in range(0,5000):
-                        ac2 = str("".join([chr(i) if 32 <= i <= 127 else "." for i in b])) # ascii string; chained comparison
+                with open(gameeboot, "rb") as ebfile:
+                    for number in range(0,50000):
+                        ac2 = str("".join([chr(i) if 32 <= i <= 127 else "." for i in ebfile.read(16)])) # ascii string; chained comparison
                         if not ac2.count('.') > 6:
                             if re.search(regex, ac2):
                                 self.ebootid = re.findall(regex, ac2)
                                 self.ebootid = self.ebootid[0].replace('_', '-')
                                 for _id in partids:
-                                    for i, n in eval(_id).items():
-                                        if i == self.ebootid:
-                                            self.rename(n)
-                        n += 1
-                        b = f.read(16)
+                                    for gid, name in eval(_id).items():
+                                        self.gid = gid
+                                        self.name = name
+                                        if self.gid == self.ebootid:
+                                            self.rename()
             except Exception as e:
-                # print(__file__, ": ", type(e).__name__, " - ", e, sep="", file=sys.stderr)
                 pass
         
-    def rename(self, n):
-        print(self.ebootid, n)
+    def rename(self):
+        gameid = self.ebootid
+        gamename = self.name
+
+        if not args.name or args.idend:
+            dest_name = f'[{gameid}] {gamename}'
+            pass
+
+        if args.name:
+            dest_name = f'{self.name}'
+        if args.idend:
+            dest_name = f'{self.name} [{gameid}]'
+            pass
+            
+        if not self.ebfilepath == os.path.join(args.PATH, dest_name + '.PBP'):
+            print("OLD NAME: " + self.ebfilepath)
+            print("NEW NAME: " + os.path.join(args.PATH, dest_name + '.PBP'))
+            print("")
+            os.rename(self.ebfilepath, os.path.join(args.PATH, dest_name + '.PBP'))
+            pass
         pass
 
 if __name__ == '__main__':
